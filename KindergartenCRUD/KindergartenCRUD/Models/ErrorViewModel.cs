a43b88c0a93d45e96653ba8ca62ab44346e505a5
qerial <avigolainen@gmail.com>
2026-09-22 18:23:07 +0300

    InitialCommit

---
+++ KindergartenCRUD/KindergartenCRUD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KindergartenCRUD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
