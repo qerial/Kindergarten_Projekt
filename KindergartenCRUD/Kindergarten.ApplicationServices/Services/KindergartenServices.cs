57ad04b02bea30437c68a193aa743f557b35e7aa
qerial <avigolainen@gmail.com>
2026-09-22 19:00:25 +0300

    Added Libaries, Folders, cs's and controller

---
+++ KindergartenCRUD/Kindergarten.ApplicationServices/Services/KindergartenServices.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kindergarten.ApplicationServices.Services
+{
+    internal class KindergartenServices
+    {
+    }
+}
---
+++ KindergartenCRUD/Kindergarten.Core/Domain/Kindergarten.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kindergarten.Core.Domain
+{
+    internal class Kindergarten
+    {
+    }
+}
