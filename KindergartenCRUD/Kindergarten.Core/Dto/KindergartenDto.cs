3feb7ac49bb197f1cb87cd77678382ebbd8af33b
qerial <avigolainen@gmail.com>
2026-09-22 19:15:54 +0300

    Added variables to Dto

--- KindergartenCRUD/Kindergarten.Core/Dto/KindergartenDto.cs
+++ KindergartenCRUD/Kindergarten.Core/Dto/KindergartenDto.cs
@@ -6,5 +6,12 @@ namespace Kindergarten.Core.Dto
 {
     public class KindergartenDto
     {
+        public Guid Id { get; set; }
+        public string GroupName { get; set; } = string.Empty;
+        public int? ChildrenCount { get; set; }
+        public string KindergartenName { get; set; } = string.Empty;
+        public string TeacherName { get; set; } = string.Empty;
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
     }
 }
